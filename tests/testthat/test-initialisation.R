5c6c2f59200282d68b1bf6d5ee88c6182e3ba4c4
adimajo <ztech1.25@gmail.com>
2020-08-19 14:14:33 +0200

    delete doublon ; added .github to buildignore

--- tests/testthat/test-initialisation.R
+++
@@ -1,8 +0,0 @@
-context("test-initialisation")
-
-test_that("generateData errors with invalid input", {
-  init <- initialization(K = 3, n = 9)
-  expect_length(init, 9)
-  expect_equal(class(init), "factor")
-  expect_equal(levels(init), c("1", "2", "3"))
-})
